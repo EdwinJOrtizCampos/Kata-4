76ecf37de3af98b1d025268dd51796945c313e7c
EdwinJOrtizCampos <edwinjavierortizcamposORCA1@HOTMAIL.COM>
2021-11-03 11:04:48 +0000

    Segunda versión de Kata4

--- src/kata4/kata4.java
+++ src/kata4/kata4.java
@@ -14,17 +14,45 @@ import kata4.view.MailListReader;
  * @author Edwin Ortiz Campos
  */
 public class kata4 {
-
+    private String path;
+    
+    public kata4(String path){
+        this.path = path;
+    }
+        
+    public void execute(){
+        List<Mail> readMails = input(path);
+        HistogramDisplay histoDisplay = process(readMails);
+        output(histoDisplay);
+        
+    }
+    
+    public List<Mail> input(String path){
+      List<Mail> mailList = MailListReader.read(path);
+      return mailList;
+    }
+    
+    public HistogramDisplay process(List<Mail> readMails){
+        Histogram<String> histogram = MailHistogramBuilder.build(readMails);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram); 
+        return histoDisplay;
+    }
+    
+    public void output(HistogramDisplay histoDisplay){
+        histoDisplay.execute();
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String filename = "C:\\Users\\edwin\\OneDrive\\Documentos\\NetBeansProjects\\Kata4\\email.txt";
-        List<Mail> mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram); 
-        histoDisplay.execute();
+        kata4 kata = new kata4("C:\\Users\\edwin\\OneDrive\\Documentos\\NetBeansProjects\\Kata4\\email.txt");
+        kata.execute();
     }
+    
+
     
     
 }
